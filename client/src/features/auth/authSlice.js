6619bf96007fc2cb19354527ae3f2155079fcd17
DEADHEAD652 <hamzajjmalik@gmail.com>
2022-12-01 01:15:47 +0500

    reducer redux

--- client/src/features/auth/authSlice.js
+++ client/src/features/auth/authSlice.js
@@ -15,10 +15,13 @@ export const authSlice = createSlice({
   reducers: {
     reset: (state) => {
       state.isLoading = false;
-      (state.isError = false), (state.isSuccess = false), (state.message = "");
+      state.isError = false;
+      state.isSuccess = false;
+      state.message = "";
     },
   },
   extraReducers: () => {},
 });
 
+export const { reset } = authSlice.actions;
 export default authSlice.reducer;
