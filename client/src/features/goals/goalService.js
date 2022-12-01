a139044fe6e76c65926ebda8e12b581138d4c0b3
DEADHEAD652 <hamzajjmalik@gmail.com>
2022-12-02 01:57:36 +0500

    fetching goals from server

--- client/src/features/goals/goalService.js
+++ client/src/features/goals/goalService.js
@@ -1,3 +1,4 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const API_URL = "/api/goals/";
@@ -14,7 +15,20 @@ const createGoal = async (goalData, token) => {
 
   return response.data;
 };
+//get goals
 
-const goalService = { createGoal };
+const getGoals = async (token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const response = await axios.get(API_URL, config);
+
+  return response.data;
+};
+
+const goalService = { createGoal, getGoals };
 
 export default goalService;
